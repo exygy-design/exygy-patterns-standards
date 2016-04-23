3d4f3725b95b77e9e9b978466cf8dc867d98b6a1
Grant Kinney <grant@exygy.com>
2016-04-22 19:43:50 -0500

    Updates scss and js paths to foundation framework

--- src/assets/toolkit/scripts/toolkit.js
+++ src/assets/toolkit/scripts/toolkit.js
@@ -15,16 +15,16 @@ require('babel-polyfill');
 // Because Bower does not force a module structure, you have use a more specific path.
 
 // Loading foundation from node
-var foundation = require('../../vendor/foundation/js/foundation.core.js');
-var foundationMediaQuery = require('../../vendor/foundation/js/foundation.util.mediaQuery.js');
-var foundationKeyboard = require('../../vendor/foundation/js/foundation.util.keyboard.js');
-var foundationBox = require('../../vendor/foundation/js/foundation.util.box.js');
-var foundationTriggers = require('../../vendor/foundation/js/foundation.util.triggers.js');
-var foundationDropdown = require('../../vendor/foundation/js/foundation.dropdown.js');
-var foundationAccordion = require('../../vendor/foundation/js/foundation.accordion.js');
-var foundationReveal = require('../../vendor/foundation/js/foundation.reveal.js');
-var foundationAbide = require('../../vendor/foundation/js/foundation.abide.js');
-var foundationTooltip = require('../../vendor/foundation/js/foundation.tooltip.js');
+var foundation = require('../../vendor/foundation-sites/js/foundation.core.js');
+var foundationMediaQuery = require('../../vendor/foundation-sites/js/foundation.util.mediaQuery.js');
+var foundationKeyboard = require('../../vendor/foundation-sites/js/foundation.util.keyboard.js');
+var foundationBox = require('../../vendor/foundation-sites/js/foundation.util.box.js');
+var foundationTriggers = require('../../vendor/foundation-sites/js/foundation.util.triggers.js');
+var foundationDropdown = require('../../vendor/foundation-sites/js/foundation.dropdown.js');
+var foundationAccordion = require('../../vendor/foundation-sites/js/foundation.accordion.js');
+var foundationReveal = require('../../vendor/foundation-sites/js/foundation.reveal.js');
+var foundationAbide = require('../../vendor/foundation-sites/js/foundation.abide.js');
+var foundationTooltip = require('../../vendor/foundation-sites/js/foundation.tooltip.js');
 
 //require('smoothstate/jquery.smoothState.min.js');
 // var smoothState = require('./jquery.smoothState.min.js');
@@ -34,7 +34,7 @@ var foundationTooltip = require('../../vendor/foundation/js/foundation.tooltip.j
 
 // Use for custom Pattern Libary Modules
 // var fooModule = require('./foo-module');
-// var bar = fooModule.foo(); 
+// var bar = fooModule.foo();
 
 // Finally, you can drop test JavaScript here...
 $(document).ready(function () {
