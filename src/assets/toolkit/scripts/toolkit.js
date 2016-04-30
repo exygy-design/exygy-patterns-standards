41c31dbffa22cc670005216b67cdc97327e4aab1
slowbot <jesse@slowbot.com>
2016-04-30 16:44:15 -0700

    adding baseline foundation components

--- src/assets/toolkit/scripts/toolkit.js
+++ src/assets/toolkit/scripts/toolkit.js
@@ -19,12 +19,16 @@ var foundation = require('../../vendor/foundation-sites/js/foundation.core.js');
 var foundationMediaQuery = require('../../vendor/foundation-sites/js/foundation.util.mediaQuery.js');
 var foundationKeyboard = require('../../vendor/foundation-sites/js/foundation.util.keyboard.js');
 var foundationBox = require('../../vendor/foundation-sites/js/foundation.util.box.js');
+var foundationBox = require('../../vendor/foundation-sites/js/foundation.util.nest.js');
+var foundationMotion = require('../../vendor/foundation-sites/js/foundation.util.motion.js');
 var foundationTriggers = require('../../vendor/foundation-sites/js/foundation.util.triggers.js');
 var foundationDropdown = require('../../vendor/foundation-sites/js/foundation.dropdown.js');
 var foundationAccordion = require('../../vendor/foundation-sites/js/foundation.accordion.js');
 var foundationReveal = require('../../vendor/foundation-sites/js/foundation.reveal.js');
 var foundationAbide = require('../../vendor/foundation-sites/js/foundation.abide.js');
 var foundationTooltip = require('../../vendor/foundation-sites/js/foundation.tooltip.js');
+var foundationDropdownMenu = require('../../vendor/foundation-sites/js/foundation.dropdownMenu.js');
+var foundationToggler = require('../../vendor/foundation-sites/js/foundation.toggler.js');
 
 //require('smoothstate/jquery.smoothState.min.js');
 // var smoothState = require('./jquery.smoothState.min.js');
